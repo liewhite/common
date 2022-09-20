98c77f0087ed9e9784ee80afa660a29c1d9ad2c8
lilin <leeliewhite@gmail.com>
2022-09-20 15:11:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Common libs for liewhite scala series
---
+++ src/main/scala/io/github/liewhite/common/SummonUtils.scala
@@ -0,0 +1,28 @@
+package io.github.liewhite.common
+
+import scala.compiletime.*
+import scala.reflect.ClassTag
+
+object SummonUtils:
+  type Head[T] = T match { case h *: t => h }
+  type Tail[T] = T match { case h *: t => t }
+  type LiftP[F[_], T] <: Tuple =
+    T match {
+      case _ *: _ => F[Head[T]] *: LiftP[F, Tail[T]]
+      case _ => EmptyTuple
+    }
+  inline def summonAll[F[_], T <: Tuple]: List[F[Any]] = 
+    inline erasedValue[T] match
+        case _: (t *: ts) => summonInline[F[t]].asInstanceOf[F[Any]] :: summonAll[F,ts]
+        case _: EmptyTuple => Nil
+
+
+  inline def summonValuesAsArray[T <: Tuple, E: ClassTag]: Array[E] =
+    summonValuesAsArray0[T, E](0, new Array[E](constValue[Tuple.Size[T]]))
+
+  inline def summonValuesAsArray0[T, E](i: Int, arr: Array[E]): Array[E] = inline erasedValue[T] match {
+    case _: EmptyTuple => arr
+    case _: (a *: b) =>
+      arr(i) = constValue[a & E]
+      summonValuesAsArray0[b, E](i+1, arr)
+  }
